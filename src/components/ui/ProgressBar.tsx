95334a7df791deaedf0f85770315ee0eba7cc62e
KNSN92 <kekenshin92@gmail.com>
2026-03-11 11:39:35 +0900

    update: transition-allを追加

--- src/components/ui/ProgressBar.tsx
+++ src/components/ui/ProgressBar.tsx
@@ -12,7 +12,10 @@ export function ProgressBar({ max, value, bgClassName, fgClassName }: Props) {
   return (
     <div className={cn("w-full h-3 bg-fg rounded-full", bgClassName)}>
       <div
-        className={cn("h-full bg-blue-500 rounded-full", fgClassName)}
+        className={cn(
+          "h-full bg-blue-500 rounded-full transition-all",
+          fgClassName,
+        )}
         style={{ width: `${progress}%` }}
       />
     </div>
