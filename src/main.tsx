5494ef8005f9761aadd24b1962942d7e8491a516
KNSN92 <kekenshin92@gmail.com>
2026-03-11 12:55:45 +0900

    fix: ビルド時にasync使っちゃだめって言われたから従う

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,29 @@ import React from "react";
 
 import { getCurrentWindow } from "@tauri-apps/api/window";
 
-let App;
-const label = await getCurrentWindow().label;
-switch (label) {
-  case "qr_maker":
-    App = (await import("./window/qr_maker")).App;
-    break;
-  case "updater":
-    App = (await import("./window/updater")).App;
-    break;
-  default:
-    App = () => <div>Unknown window</div>;
-    break;
+function View() {
+  const [View, setView] = React.useState<React.FC | null>(null);
+  const label = getCurrentWindow().label;
+  switch (label) {
+    case "qr_maker":
+      import("./view/qr_maker").then((module) =>
+        setView(() => module.QRMakerView),
+      );
+      break;
+    case "updater":
+      import("./view/updater").then((module) =>
+        setView(() => module.UpdaterView),
+      );
+      break;
+    default:
+      setView(() => <div>Unknown window</div>);
+      break;
+  }
+  return View && <View />;
 }
 
 ReactDom.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <View />
   </React.StrictMode>,
 );
