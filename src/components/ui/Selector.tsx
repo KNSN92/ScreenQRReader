bc05b89e6fbf2c71554c38331c4e4adac4633d85
KNSN92 <kekenshin92@gmail.com>
2026-03-19 19:53:28 +0900

    update: cursor pointerを追加

--- src/components/ui/Selector.tsx
+++ src/components/ui/Selector.tsx
@@ -43,12 +43,15 @@ export function Selector({
       className={cn("w-40", className)}
       classNames={{
         container: () => "rounded-lg text-2xl shadow-md shadow-black/20",
-        control: () => "h-10 px-3 bg-fg hover:bg-fg-dark rounded-lg text-white",
+        control: () =>
+          "h-10 px-3 bg-fg hover:bg-fg-dark rounded-lg text-white cursor-pointer!",
         menu: () => "rounded-lg py-2 bg-fg text-white",
         option: (props) =>
           cn(
             "flex justify-center items-center h-10 px-3 transition-all",
-            props.isSelected ? "bg-fg-dark" : "hover:bg-primary",
+            props.isSelected
+              ? "bg-fg-dark"
+              : "hover:bg-primary cursor-pointer!",
           ),
       }}
       unstyled
